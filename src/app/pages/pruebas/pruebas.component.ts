9d6958b9bcb09be1f8cdee728f97d73da9f762ea
oscar-quiroz <kairox.rt@gmail.com>
2021-07-11 17:06:24 -0500

    carga de numeros pseudoaleatorios

--- src/app/pages/pruebas/pruebas.component.ts
+++ src/app/pages/pruebas/pruebas.component.ts
@@ -3,13 +3,35 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-pruebas',
   templateUrl: './pruebas.component.html',
-  styleUrls: ['./pruebas.component.css']
+  styleUrls: ['./pruebas.component.css'],
 })
 export class PruebasComponent implements OnInit {
+  inputFile: any;
+  file: any;
+  auxInputFile: string = '';
+  resultFile: any;
+  ok = '';
+  lsita = [];
 
-  constructor() { }
+  numeros: any;
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  leerArchivo(e) {
+    this.ok = 'validate';
+    this.auxInputFile = this.inputFile;
+    let archivo = e.target.files[0];
+    if (!archivo) {
+      return;
+    }
+    let lector = new FileReader();
+    lector.onload = (e) => {
+      let contenido = e.target.result;
+      this.numeros = contenido;
+
+      this.lsita = this.numeros.split(',');
+    };
+    lector.readAsText(archivo);
   }
-
 }
